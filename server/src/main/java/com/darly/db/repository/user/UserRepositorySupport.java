ad8c8f7b036eca999254c3ccecd82bb34d0f733d
박성호 <dkdlah0830@naver.com>
2022-04-22 15:03:37 +0900

    remove : server derectory

--- server/src/main/java/com/darly/db/repository/user/UserRepositorySupport.java
+++
@@ -1,8 +0,0 @@
-package com.darly.db.repository.user;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class UserRepositorySupport {
-
-}
--- server/src/main/java/com/darly/db/repository/userAddress/UserAddressRepository.java
+++
@@ -1,8 +0,0 @@
-package com.darly.db.repository.userAddress;
-
-import com.darly.db.entity.userAddress.UserAddress;
-import com.darly.db.entity.userAddress.UserAddressId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAddressRepository extends JpaRepository<UserAddress, UserAddressId> {
-}
