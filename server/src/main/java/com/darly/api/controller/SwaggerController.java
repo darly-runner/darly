1be3f9deddbc6574aade6ae795dd8615899e1925
KimMinKwan <zoog15@naver.com>
2022-04-25 16:45:32 +0900

    docs: 젠킨스 자동배포 테스트2

--- server/src/main/java/com/darly/api/controller/SwaggerController.java
+++ server/src/main/java/com/darly/api/controller/SwaggerController.java
@@ -14,7 +14,8 @@ public class SwaggerController {
     // 기존의 주소를 좀더 간결하게 입력해서 들어가려고 만든 함수
     @GetMapping("/swagger")
     public void home(HttpServletResponse response) throws IOException {
-        String redirect_url = "http://localhost:8080/swagger-ui/index.html#";
+//        String redirect_url = "http://localhost:8080/swagger-ui/index.html#"; // 로컬에서 테스트할떄 리다이렉팅
+        String redirect_url = "http://3.36.61.107:8080/swagger-ui/index.html#"; // ec2에서 리다이렉팅
         response.sendRedirect(redirect_url);
     }
 }
