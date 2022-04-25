5d050d9b7f18fc1e1bf4917593429315b4c96d37
박성호 <dkdlah0830@naver.com>
2022-04-25 14:51:46 +0900

    feat : 카카오 로그인

--- Android/app/src/main/java/com/ssafy/darly/fragment/MypageFragment.kt
+++ Android/app/src/main/java/com/ssafy/darly/fragment/MypageFragment.kt
@@ -1,19 +1,30 @@
 package com.ssafy.darly.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.kakao.sdk.user.UserApiClient
 import com.ssafy.darly.R
+import com.ssafy.darly.activity.LoginActivity
 import com.ssafy.darly.databinding.FragmentMypageBinding
 import com.ssafy.darly.viewmodel.MypageViewModel
 
 class MypageFragment : Fragment() {
     private lateinit var binding: FragmentMypageBinding
     private val model: MypageViewModel by viewModels()
+
+    var auth : FirebaseAuth?= null
+    var googleSignInClient : GoogleSignInClient?= null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +36,35 @@ class MypageFragment : Fragment() {
             binding.viewModel = model
         }
 
+        logout()
+
         return binding.root
+    }
+
+    fun logout(){
+        // 구글 로그아웃을 위해 로그인 세션 가져오기
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id2))
+            .requestEmail()
+            .build()
+        googleSignInClient = this.context?.let { GoogleSignIn.getClient(it, gso) }
+
+        // firebaseauth를 사용하기 위한 인스턴스 get
+        auth = FirebaseAuth.getInstance()
+
+        // 구글 로그아웃 버튼 클릭 시 이벤트
+        binding.googleLogout.setOnClickListener {
+            UserApiClient.instance.logout { error ->
+                if (error == null)
+                    Toast.makeText(context, "로그아웃 성공", Toast.LENGTH_SHORT).show()
+            }
+
+            FirebaseAuth.getInstance().signOut()
+            googleSignInClient?.signOut()
+
+            var logoutIntent = Intent(this.context, LoginActivity::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(logoutIntent)
+        }
     }
 }
---
+++ Android/app/src/main/java/com/ssafy/darly/util/GlobalApplication.kt
@@ -0,0 +1,11 @@
+package com.ssafy.darly.util
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this, "3a20416392ebf79f9356a5ebfd94116a")
+    }
+}
