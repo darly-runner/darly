460ab1571e24c79d52e8f8013c51ba32a82611db
rosieyeon <sseyeon956@gmail.com>
2022-05-06 23:46:24 +0900

    fix: remmended crew list spacing

---
+++ Android/app/src/main/java/com/ssafy/darly/AllCrewListActivity.kt
@@ -0,0 +1,11 @@
+package com.ssafy.darly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllCrewListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_crew_list)
+    }
+}
