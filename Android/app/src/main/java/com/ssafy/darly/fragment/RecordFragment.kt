25351bd9838d40300c9dce921c000dcbc4d90d29
chung1997jy <chung1997jy@gmail.com>
2022-05-11 02:13:47 +0900

    [#S06P31A301-214] feat: record all list page

--- Android/app/src/main/java/com/ssafy/darly/fragment/RecordFragment.kt
+++ Android/app/src/main/java/com/ssafy/darly/fragment/RecordFragment.kt
@@ -1,5 +1,6 @@
 package com.ssafy.darly.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ssafy.darly.R
+import com.ssafy.darly.activity.FriendActivity
+import com.ssafy.darly.activity.RecordAllActivity
 import com.ssafy.darly.adapter.record.RecordListAdapter
 import com.ssafy.darly.databinding.FragmentRecordBinding
 import com.ssafy.darly.service.DarlyService
@@ -34,6 +37,11 @@ class RecordFragment : Fragment() {
         recordListAdapter = RecordListAdapter()
         binding.recyclerView.adapter = recordListAdapter
         binding.recyclerView.layoutManager = LinearLayoutManager(context)
+
+        binding.allBtn.setOnClickListener {
+            val intent = Intent(this.requireContext(), RecordAllActivity::class.java)
+            startActivity(intent)
+        }
 
         return binding.root
     }
