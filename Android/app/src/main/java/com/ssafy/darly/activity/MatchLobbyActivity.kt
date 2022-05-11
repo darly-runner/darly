100e09859f23d6440e15b6b63d22d68d32e0f4cc
rosieyeon <sseyeon956@gmail.com>
2022-05-11 18:33:37 +0900

    add: websocket connect

--- Android/app/src/main/java/com/ssafy/darly/activity/MatchLobbyActivity.kt
+++ Android/app/src/main/java/com/ssafy/darly/activity/MatchLobbyActivity.kt
@@ -15,15 +15,84 @@ import com.ssafy.darly.databinding.ActivityMatchLobbyBinding
 import com.ssafy.darly.model.MatchUsers
 import com.ssafy.darly.service.DarlyService
 import com.ssafy.darly.viewmodel.CrewViewModel
+import io.reactivex.disposables.Disposable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.DisposableHandle
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import org.json.JSONObject
+import ua.naiksoftware.stomp.Stomp
+import ua.naiksoftware.stomp.StompClient
+import ua.naiksoftware.stomp.dto.LifecycleEvent
+import ua.naiksoftware.stomp.dto.StompCommand
+import ua.naiksoftware.stomp.dto.StompHeader
 
 class MatchLobbyActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMatchLobbyBinding
     private val model: CrewViewModel by viewModels()
     var matchId: Long = 0
     lateinit var adapter: CrewMatchLobbyAdapter
+
+//    lateinit var stompConnection: Disposable
+//    lateinit var topic: Disposable
+
+    val url = "ws://3.36.61.107:8000/ws"
+//    val intervalMills = 1000L
+//    val client = OkHttpClient()
+//    lateinit var stomp: StompClient
+
+//    val stomp = StompClient()
+//    val test = StompCommand()
+
+    val stompClient =  Stomp.over(Stomp.ConnectionProvider.OKHTTP, url)
+
+    fun runStomp() {
+//        stompClient.
+//        stompClient.topic("/chat/enter").subscribe { topicMessage ->
+//            Log.i("message Recieve", topicMessage.payload)
+//        }
+
+//        val headerList = arrayListOf<StompHeader>()
+//        headerList.add(StompHeader("inviteCode","test0912"))
+//        headerList.add(StompHeader("username", text.value))
+//        headerList.add(StompHeader("positionType", "1"))
+//        stompClient.connect(headerList)
+        stompClient.connect()
+
+        stompClient.lifecycle().subscribe { lifecycleEvent ->
+            when (lifecycleEvent.type) {
+                LifecycleEvent.Type.OPENED -> {
+                    Log.d("ENTER", "ENTERED!!")
+                }
+                LifecycleEvent.Type.ERROR -> {
+                    Log.i("ERROR", "!!")
+                    Log.e("CONNECT ERROR", lifecycleEvent.exception.toString())
+                }
+            }
+        }
+//        stompClient.lifecycle().subscribe { lifecycleEvent ->
+//            when (lifecycleEvent.type) {
+//                LifecycleEvent.Type.OPENED -> {
+//                    Log.i("ENTER", "!!")
+//                }
+//                LifecycleEvent.Type.ERROR -> {
+//                    Log.i("ERROR", "!!")
+//                    Log.e("CONNECT ERROR", lifecycleEvent.exception.toString())
+//                }
+//                else ->{
+//                    Log.i("ELSE", lifecycleEvent.message)
+//                }
+//            }
+//        }
+//        val data = JSONObject()
+//        data.put("positionType", "1")
+//        data.put("content", "test")
+//        data.put("messageType", "CHAT")
+//        data.put("destRoomCode", "test0912")
+//
+//        stompClient.send("/stream/chat/send", data.toString()).subscribe()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +112,7 @@ class MatchLobbyActivity : AppCompatActivity() {
 //        binding.matchUsersList.adapter = adapter
 //        binding.matchUsersList.layoutManager = GridLayoutManager(this, 1)
 //        subscribeObserver()
-
+        runStomp()
         CoroutineScope(Dispatchers.Main).launch {
             val response = DarlyService.getDarlyService().getMatchDetails(matchId)
             model.matchUsers.value = response.body()?.users ?: listOf()
