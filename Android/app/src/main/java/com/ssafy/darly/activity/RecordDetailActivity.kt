473f56d779ba11cef205980cabb22a7623ed8ea4
chung1997jy <chung1997jy@gmail.com>
2022-05-11 18:57:58 +0900

    wip: detail record binding

--- Android/app/src/main/java/com/ssafy/darly/activity/RecordDetailActivity.kt
+++ Android/app/src/main/java/com/ssafy/darly/activity/RecordDetailActivity.kt
@@ -1,14 +1,12 @@
 package com.ssafy.darly.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.common.util.DataUtils
 import com.ssafy.darly.R
 import com.ssafy.darly.databinding.ActivityRecordDetailBinding
 import com.ssafy.darly.model.record.RecordDetailGetRes
-import com.ssafy.darly.model.stat.StatGetRes
 import com.ssafy.darly.service.DarlyService
 import com.ssafy.darly.viewmodel.RecordDetailViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -29,23 +27,25 @@ class RecordDetailActivity : AppCompatActivity() {
 
         recordId = intent.getLongExtra("recordId", 0L)
         CoroutineScope(Dispatchers.Main).launch {
-            val response = DarlyService.getDarlyService().getRecordDetail(recordId)
-//            model.records.value = response.body()?.records ?: listOf()
-//            recordListAdapter.notifyDataSetChanged()
+            setModelData(DarlyService.getDarlyService().getRecordDetail(recordId))
         }
     }
 
-//    private fun setModelData(response: Response<RecordDetailGetRes>) {
-//        model.recordTitle.value
-//        model.totalDistance.value = response.body()?.totalDistance?.toString() ?: "0"
-//        model.totalNum.value = response.body()?.totalNum?.toString() ?: "0"
-//        val totalSecs = response.body()?.totalTime ?: 0
-//        model.totalTime.value = String.format("%02d:%02d:%02d", totalSecs / 3600, (totalSecs % 3600) / 60, totalSecs % 60)
-//        val paceAvg: Int = response.body()?.paceAvg?.toInt() ?: 0
-//        model.paceAvg.value = String.format("%01d'%02d''", paceAvg / 60, paceAvg % 60)
-//        model.heartAvg.value = response.body()?.heartAvg?.toString()
-//        if (model.heartAvg.value == null || model.heartAvg.value == "0")
-//            model.heartAvg.value = "--"
-//        model.distances.value = response.body()?.distances ?: listOf()
-//    }
+    private fun setModelData(response: Response<RecordDetailGetRes>) {
+        model.recordTitle.value = response.body()?.recordTitle
+        model.recordDate.value = response.body()?.recordDate
+        model.recordDistance.value = String.format("%02f", response.body()?.recordDistance)
+        val totalSecs = response.body()?.recordTime ?: 0
+        model.recordTime.value = String.format("%02d:%02d:%02d", totalSecs / 3600, (totalSecs % 3600) / 60, totalSecs % 60)
+        val pace: Int = response.body()?.recordPace ?: 0
+        model.recordPace.value = String.format("%01d'%02d''", pace / 60, pace % 60)
+        model.recordHeart.value = response.body()?.recordHeart?.toString()
+        if (model.recordHeart.value == null || model.recordHeart.value == "0")
+            model.recordHeart.value = "--"
+        model.recordSpeed.value = response.body()?.recordSpeed?.toString()
+        model.recordCalories.value = response.body()?.recordCalories?.toString()
+        model.coordinateLongitudes.value = response.body()?.coordinateLongitudes
+        model.coordinateLatitudes.value = response.body()?.coordinateLatitudes
+        model.recordRank.value = response.body()?.recordRank.toString()
+    }
 }
