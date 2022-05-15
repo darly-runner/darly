c0a2767b6e65d0a194e3a7b8d35d067a4644fd93
박성호 <dkdlah0830@naver.com>
2022-05-15 17:38:30 +0900

    feat : 워치 백그라운드 오류 해결 및 대기모드 추가

--- Android/app/src/main/java/com/ssafy/darly/activity/MatchActivity.kt
+++ Android/app/src/main/java/com/ssafy/darly/activity/MatchActivity.kt
@@ -2,16 +2,13 @@ package com.ssafy.darly.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
-import com.kakao.util.helper.Utility
 import com.ssafy.darly.R
 import com.ssafy.darly.adapter.match.MatchViewPagerAdapter
 import com.ssafy.darly.databinding.ActivityMatchBinding
 import com.ssafy.darly.model.CompetitorInfo
 import com.ssafy.darly.model.RecordRequest
-import com.ssafy.darly.model.record.Record
 
 import com.ssafy.darly.viewmodel.RunningViewModel
 
@@ -35,36 +32,24 @@ class MatchActivity : AppCompatActivity() {
         binding.matchViewPager.adapter = adapter
 
         val list = mutableListOf<CompetitorInfo>()
-        list.add(CompetitorInfo(
-            RecordRequest(
-                null,
-                0f,
-                0,
-                0,
-                0,
-                0f,
-                0,
-                null,
-                null,
-                ArrayList(),
-                ArrayList(),
-                ArrayList()
-            )))
-        list.add(CompetitorInfo(
-            RecordRequest(
-                null,
-                0f,
-                0,
-                0,
-                0,
-                0f,
-                0,
-                null,
-                null,
-                ArrayList(),
-                ArrayList(),
-                ArrayList()
-            )))
+
+        for(i in 0 until 3){
+            list.add(CompetitorInfo(
+                RecordRequest(
+                    null,
+                    0f,
+                    0,
+                    0,
+                    0,
+                    0f,
+                    0,
+                    null,
+                    null,
+                    ArrayList(),
+                    ArrayList(),
+                    ArrayList()
+                )))
+        }
         adapter.list = list
         adapter.notifyDataSetChanged()
     }
