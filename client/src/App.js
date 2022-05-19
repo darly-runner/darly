6e6f75f7e6c0e138426e4e64597afa880f08cb70
rosieyeon <sseyeon956@gmail.com>
2022-05-19 18:27:22 +0900

    wip: download apk file

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {
   Activity,
   Compete,
   Crew,
+  Download,
   Friends,
   Header,
   Layout,
@@ -16,12 +17,14 @@ function App() {
     <div className="App">
       <Header>
         <Icon icon="darlyBig" width={130} height={50} viewBox="0 0 507 207" />
-        <BasicButton width={100} height={50} fontSize="16px">
-          <a href="https://darly-bucket.s3.ap-northeast-2.amazonaws.com/darly.apk">
+        <Download href="https://darly-bucket.s3.ap-northeast-2.amazonaws.com/darly.apk">
+          {/* <a href="https://darly-bucket.s3.ap-northeast-2.amazonaws.com/darly.apk"> */}
+          <BasicButton width={100} height={50} fontSize="16px">
             앱 다운로드
-          </a>
-          <Icon icon="download" width={24} height={24} viewBox="0 0 24 24" />
-        </BasicButton>
+            <Icon icon="download" width={24} height={24} viewBox="0 0 24 24" />
+          </BasicButton>
+          {/* </a> */}
+        </Download>
       </Header>
 
       <Layout>
