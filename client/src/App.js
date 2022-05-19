b272a922e983c32be1a72fca46477af00b867902
rosieyeon <sseyeon956@gmail.com>
2022-05-19 18:28:03 +0900

    style: delete annotations

--- client/src/App.js
+++ client/src/App.js
@@ -18,12 +18,10 @@ function App() {
       <Header>
         <Icon icon="darlyBig" width={130} height={50} viewBox="0 0 507 207" />
         <Download href="https://darly-bucket.s3.ap-northeast-2.amazonaws.com/darly.apk">
-          {/* <a href="https://darly-bucket.s3.ap-northeast-2.amazonaws.com/darly.apk"> */}
           <BasicButton width={100} height={50} fontSize="16px">
             앱 다운로드
             <Icon icon="download" width={24} height={24} viewBox="0 0 24 24" />
           </BasicButton>
-          {/* </a> */}
         </Download>
       </Header>
 
