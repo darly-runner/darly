88745805a46950dc925a9e83607c79632a3042a9
rosieyeon <sseyeon956@gmail.com>
2022-05-19 11:04:35 +0900

    wip: change favicon & app name

---
+++ client/src/App.styled.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Main = styled.div`
+  width: 100vh;
+  height: 784px;
+  background-color: #fff8f8;
+`;
+
+export const Crew = styled.div`
+  width: 100vh;
+  height: 784px;
+`;
+
+export const Compete = styled.div`
+  width: 100vh;
+  height: 784px;
+  background-color: #fff8f8;
+`;
+
+export const Activity = styled.div`
+  width: 100vh;
+  height: 784px;
+`;
+
+export const Friends = styled.div`
+  width: 100vh;
+  height: 784px;
+  background-color: #fff8f8;
+`;
