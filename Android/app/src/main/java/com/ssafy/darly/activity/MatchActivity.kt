fd850bc4f885f9dce0c1f9797471bccb99a35ec5
chung1997jy <chung1997jy@gmail.com>
2022-05-20 19:10:51 +0900

    hotfix: target distance 1000f

--- Android/app/src/main/java/com/ssafy/darly/activity/MatchActivity.kt
+++ Android/app/src/main/java/com/ssafy/darly/activity/MatchActivity.kt
@@ -48,7 +48,7 @@ class MatchActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var myUserId: Long = 0
     private var crewId: Long = 0
     private var url = "http://3.36.61.107:8000/ws/websocket"
-    private val targetDistance = 100f
+    private val targetDistance = 1000f
     private var isEnd = false;
     val stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, url)
 
